0aa9e15d5e2ccbea8d494e15f5907b23ae36e542
Satyam Chaurasia <chaurasiasatyam981@gmail.com>
2020-11-29 17:29:50 +0530

    Updates

---
+++ precache-manifest.e6eb577ac0e3f08cb09af962e2f46a62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5473da0f7ccd6b6854d487f5e15c5f89",
+    "url": "/Forms-assignment/index.html"
+  },
+  {
+    "revision": "8bf7ce83eb91f52be086",
+    "url": "/Forms-assignment/static/css/main.2bf787f8.chunk.css"
+  },
+  {
+    "revision": "da239534b98bc4073192",
+    "url": "/Forms-assignment/static/js/2.d5ce56fb.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/Forms-assignment/static/js/2.d5ce56fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bf7ce83eb91f52be086",
+    "url": "/Forms-assignment/static/js/main.1cb5cacb.chunk.js"
+  },
+  {
+    "revision": "1ca4f811a7d329711522",
+    "url": "/Forms-assignment/static/js/runtime-main.87f117e0.js"
+  }
+]);
